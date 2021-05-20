13406346f7b843be08c6939dff5e33d70b6e1262
Ryan Haywood <ryanhaywoodj@gmail.com>
2021-05-20 08:20:29 -0400

    New recipes

--- src/dough/work_bench.clj
+++ src/dough/work_bench.clj
@@ -1,6 +1,26 @@
 (ns dough.work-bench
   (:require [dough.core :refer :all]))
 
+;; lombardi guy
+;; https://www.pizzamaking.com/forum/index.php?topic=31477.msg312872#msg312872
+(-> {:flour 1.0
+     :water 0.58
+     :sugar 0.02
+     :salt 0.02
+     :oil 0.001}
+    (gen-recipe 235)
+    (with-levain 0.02)
+    (scale 3))
+
+(def mike-essen
+  "https://www.pizzamaking.com/forum/index.php?topic=8093.msg558793#msg558793"
+  {:flour 1.0
+   :water 0.65
+   :salt 0.025
+   :oil 0.02
+   :honey 0.01
+   :sugar 0.01})
+
 ;; this was good, forgot the oil, needed more salt
 ;; https://photos.google.com/photo/AF1QipOvE0biMGOCqUXbrwHfTNBPS_PIuk19Vg3c2eTP
 ;; baked at 600/600/even heat for 4 minutes
@@ -15,7 +35,7 @@
     (scale 3))
 
 ;; FORKISH BLONDE COUNTRY
-
+;; great
 (-> {:flour 1.0
      :water 0.78
      :salt 0.022}
@@ -23,6 +43,18 @@
     (scale 2)
     (vary-flour {:whole-wheat 0.05 :rye 0.05})
     (with-levain 0.08))
+
+;; 2-24-2021
+;; 9:00 auto start
+;; actual FF 7
+(ddt 7 70 71.7 86)
+(-> {:flour 1.0
+     :water 0.78
+     :salt 0.022}
+    (gen-recipe 901)
+    (scale 2)
+    (vary-flour {:whole-wheat 0.05 :rye 0.05})
+    (with-levain 0.12))
 
 ;; FORKISH PIZZA
 ;; 20-02-2021
@@ -33,6 +65,8 @@
     (scale 3)
     (vary-flour {:whole-wheat 0.02})
     (with-levain 0.07))
+;; review: mostly trash
+;; dry? didn't brown nicely, kind of doughy
 
 ;; Noel intermediate class
 ;; levain% = idy% * 100
@@ -44,15 +78,15 @@
      :water 0.65
      :salt 0.02
      :oil 0.03
-     :sugar 0.03}
+     :sugar 0.03
+     :idy 0.002}
     (gen-recipe 262)
-    (vary-flour {:whole-wheat 0.12 :rye 0.08})
-    (with-levain 0.025)
     (scale 2))
 
 (ddt 5 71 72.5 73 75)
 
-;; 2/22/2021
+;; 2/22/2021 -- Slow Rise modded for sourdough and 20% blend of other flours
+;;
 ;; Mixed flour water oil sugar and brought to gether for first rest at 8:55
 ;; 9:08pm added salt and mixed for 4 minutes rest 5 minutes
 ;; 9:16 knead on block for 5 minutes
@@ -65,7 +99,7 @@
 ;; 11:15pm Last fold DT 74.8
 ;; 11:40am divide and preshape and rest DT 72.5
 ;; 11:52 After bench rest final shaping and putting into bowls
-;; ----
+;;
 ;; FF 5 from last time
 (ddt 5 71 72.5 73 75)
 ;; Actual friction factor 8
@@ -80,23 +114,96 @@
     (with-levain 0.025)
     (scale 2))
 
-;; lombardi guy
-;; https://www.pizzamaking.com/forum/index.php?topic=31477.msg312872#msg312872
-(-> (percentages?
-     {:flour 22679.6
-      :water 13248.94
-      :sugar 453.59
-      :salt  453.59
-      :oil   218.0})
-    (gen-recipe 235)
-    (with-levain 0.02)
+;; Mix flour water oil sugar and starter and let rest for 15 minutes
+;; add salt and mix thoroughly, rest for 5 minutes
+;; knead for 5 minutes
+;; rest for 5
+;; knead for 5 minutes
+;; rest for 5
+;; in oil and bucket, let rest for 10 minutes
+;; first fold
+;; rest 10 minutes
+;; second fold
+;; rest 10 minutes
+;; third fold
+;; rest 10 minutes
+;; fourth fold
+;; bulk fermentation continues for 12 (varys based on starter%) hours
+;; divide and preshape
+;; rest 15 minutes
+;; final shapingke
+
+;; 2/24/2021 -- Take 2
+;;
+;; 7:20 Mixed flour water oil sugar and brought to gether for first rest
+;; DT 72.1
+;; 7:35 added salt and mixed for 5 minutes
+;; 7:40 rest 5 minutes
+;; DT 72.1
+;; 7:45 knead on block for 5 minutes
+;; DT 73
+;; 7:50 rest 10 minutes
+;; 8:00 Knead on block for 8 minutes
+;; DT 76
+;; 8:10 Rest minutes
+;; 8:20 Knead for 7 minutes
+;; DT 77.9
+;; 8:28 In bucket with some oil in proofer set to 75f
+;; 8:38 first folds
+;; DT 76.1
+;; 8:50 second fold
+;; DT 78.8
+;; 9:04 third fold
+;; DT 79.7
+;; 9:16pm fourth fold
+;; 11:20am Divde and shape
+;; DT 71.6
+(ddt 5 74.3 73.2 67)
+(-> {:flour 1.0
+     :water 0.65
+     :salt 0.02
+     :oil 0.03
+     :sugar 0.03}
+    (gen-recipe 265)
+    (with-levain 0.0125)
+    (scale 2))
+
+;; 8:27 auto start
+;; 8:57 add salt and mix
+;; DT 77.1
+;; 9:07 knead for 5
+(ddt 5 74.3 71.9 73)
+(-> {:flour 1.0
+     :water 0.65
+     :salt 0.02
+     :oil 0.03
+     :sugar 0.03}
+    (gen-recipe 265)
+    (with-levain 0.0125)
+    (scale 2))
+
+100lbs of flour
+26liters of water
+(-> {:flour 45359
+     :water 26000}
+    percentages?)
+
+
+(-> {:flour 1.0
+     :water 0.58
+     :salt 0.024}
+    (gen-recipe 265)
+    (with-levain 0.005)
     (scale 3))
 
-(def mike-essen
-  "https://www.pizzamaking.com/forum/index.php?topic=8093.msg558793#msg558793"
-  {:flour 1.0
-   :water 0.65
-   :salt 0.025
-   :oil 0.02
-   :honey 0.01
-   :sugar 0.01})
+(ddt 7 72.5 70.7 75)
+
+(-> {:flour 1.0
+     :water 0.65
+     :salt 0.024
+     :oil 0.03
+     :idy 0.002}
+    (gen-recipe 310)
+    (scale 4))
+
+(ddt 6 68 72 55)
